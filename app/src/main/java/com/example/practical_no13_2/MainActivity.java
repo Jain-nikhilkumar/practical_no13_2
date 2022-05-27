d29dcd9ddb94c12a2622c505b4c79d280c63a735
Jain-nihhilkumar <nikhilkumar1514@gmail.com>
2022-05-27 19:48:52 +0530

    TESTING GITHUB CODE ADD

---
+++ app/src/main/java/com/example/practical_no13_2/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.practical_no13_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+    Button btnStartProgress;
+    ProgressDialog progressBar;
+    private int progressBarStatus = 0;
+    private Handler progressBarHandler = new Handler();
+    private long fileSize = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        addListenerOnButtonClick();
+    }
+    public void addListenerOnButtonClick() {
+        btnStartProgress = findViewById(R.id.button1);
+        btnStartProgress.setOnClickListener(new View.OnClickListener
+                (){
+            @Override
+            public void onClick(View v) {
+                progressBar = new ProgressDialog(v.getContext());
+                progressBar.setCancelable(true);
+                progressBar.setMessage("File downloading ...");
+                progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressBar.setProgress(0);
+                progressBar.setMax(100);
+                progressBar.show();
+                progressBarStatus = 0;
+                fileSize = 0;
+                new Thread(new Runnable() {
+                    public void run() {
+                        while (progressBarStatus < 100) {
+                            progressBarStatus = doOperation();
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            progressBarHandler.post(new Runnable() {
+                                public void run() {
+                                    progressBar.setProgress(progressBarStatus);
+                                }
+
+                            });
+                        }
+                        if (progressBarStatus >= 100) {
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            progressBar.dismiss();
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+    public int doOperation() {
+        while (fileSize <= 10000) {
+            fileSize++;
+            if (fileSize == 1000) {
+                return 10;
+            } else if (fileSize == 2000) {
+                return 20;
+            } else if (fileSize == 3000) {
+                return 30;
+            } else if (fileSize == 4000) {
+                return 40;
+            }
+        }
+        return 100;
+    }
+}
+
